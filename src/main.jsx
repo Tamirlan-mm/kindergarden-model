3565f38c58ff34904cf87cdcdb1fcf864961f4b5
Tamirlan Meshiev <tamirlan.meshiyev@gmail.com>
2026-03-28 04:57:25 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import KindergartenFinModel from './KindergartenFinModel.jsx'
+import KindergartenFinModel from './kindergarten.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(<KindergartenFinModel />)
